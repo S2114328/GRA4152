13d9486313a5ead06769525426049319dd0e3f88
S2114328 <s2114328@bi.no>
2022-09-27 13:48:57 +0200

    homework week 5

--- P10_8.py
+++ P10_8.py
@@ -4,11 +4,12 @@
 
 class Person:
     ## initialize variables
-    #
+    # @ name : name of person
+    # @ YoB: year of birth of person
     def __init__(self, name = "not provided", YoB = "not provided"):
         self._name = name
         self._YoB = YoB
-    ## print 
+    ## print person object
     #       
     def __repr__(self):
         return self._name + " , " + str(self._YoB)
@@ -17,18 +18,23 @@ class Person:
 
 class Student(Person):
     ##initialize variables
-    #
+    # @ name: name of person
+    # @ YoB: year of birth of person
+    # @ major: major of study
+    
     def __init__(self, name = "not provided", YoB = "not provided", major = "not provided"):
         super().__init__(name,YoB)
         self._major = major
-    ## print
+    ## print student object
     #
     def __repr__(self):
         return self._name + " , " + str(self._YoB) + " , " + self._major
         
 class Instructor(Person):
-    ## initialize variables
-    #
+    ##initialize variables
+    # @ name: name of person
+    # @ YoB: year of birth of person
+    # @ salary: salary of instructor
     def __init__(self, name = "not provided", YoB = "not provided", salary = "not provided"):
         super().__init__(name, YoB)
         self._salary = salary
